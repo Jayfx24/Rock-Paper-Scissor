4085c6106e09716f9e86f9b12de233a9f320a379
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2024-10-19 05:44:28 -0400

    added resetGame

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-let option = ["rock", "paper", "scissors"];
+const option = ["rock", "paper", "scissors"];
 let userScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 let rounds = 0;
-let usersChoice ;
+const round_left = 5
+let usersChoice;
 let computersChoice = "";
 
 let userWin = false;
@@ -15,25 +16,28 @@ const div = document.createElement("div");
 const rock = document.createElement("button");
 const paper = document.createElement("button");
 const scissors = document.createElement("button");
+const playAgain = document.createElement("button");
 //
 const h1 = document.createElement("h1");
 const p = document.createElement("p");
 //
 let gameStatus = document.createElement("p");
 let showScore = document.createElement("p");
-let winStatus = document.createElement("h3")
+let winStatus = document.createElement("h3");
 
 h1.textContent = "Welcome to the Rock Paper Scissors Game";
 p.textContent = "Choose between the buttons below";
 rock.textContent = "Rock";
 paper.textContent = "Paper";
 scissors.textContent = "Scissors";
-gameStatus.textContent ;
-winStatus.textContent ;
+playAgain.textContent = "Play Again";
+gameStatus.textContent;
+winStatus.textContent;
 
-// 
-rock.style.margin = "5px"
-scissors.style.margin = "5px"
+//
+rock.style.margin = "5px";
+scissors.style.margin = "5px";
+playAgain.style.display = "None";
 
 div.className = "btn-div";
 container.appendChild(h1);
@@ -44,9 +48,8 @@ div.appendChild(scissors);
 container.appendChild(gameStatus);
 container.appendChild(showScore);
 container.appendChild(div);
-container.appendChild(winStatus)
-
-
+container.appendChild(winStatus);
+container.appendChild(playAgain);
 
 function getUserChoice(event) {
   let a = event.target.textContent.toLowerCase();
@@ -64,38 +67,36 @@ function getComputerChoice() {
 }
 
 function playRound(userChoice, computerChoice) {
- 
-    if (userChoice == "rock" && computerChoice == "scissors") {
-      userWin = true;
-    } else if (computerChoice == "rock" && userChoice == "scissors") {
-      computerWin = true;
-    } else if (userChoice == "paper" && computerChoice == "rock") {
-      userWin = true;
-    } else if (computerChoice == "paper" && userChoice == "rock") {
-      computerWin = true;
-    } else if (userChoice == "scissors" && computerChoice == "paper") {
-      userWin = true;
-    } else if (computerChoice == "scissors" && userChoice == "paper") {
-      computerWin = true;
-    } else if (userChoice == computerChoice) {
-      draw = true;
-      drawScore += 1;
-      gameStatus.textContent = "That was a draw";
-    }
+  if (userChoice == "rock" && computerChoice == "scissors") {
+    userWin = true;
+  } else if (computerChoice == "rock" && userChoice == "scissors") {
+    computerWin = true;
+  } else if (userChoice == "paper" && computerChoice == "rock") {
+    userWin = true;
+  } else if (computerChoice == "paper" && userChoice == "rock") {
+    computerWin = true;
+  } else if (userChoice == "scissors" && computerChoice == "paper") {
+    userWin = true;
+  } else if (computerChoice == "scissors" && userChoice == "paper") {
+    computerWin = true;
+  } else if (userChoice == computerChoice) {
+    draw = true;
+    drawScore += 1;
+    gameStatus.textContent = "That was a draw";
+  }
 
-    if (userWin) {
-      userScore += 1;
-      gameStatus.textContent = `You win :) this round\nYou chose ${userChoice},computer chose ${computerChoice}`;
-    } else if (computerWin) {
-      computerScore += 1;
-      gameStatus.textContent = `You lost :( this round\nYou chose ${userChoice},computer chose ${computerChoice}`;
-    }
-    if (draw) {
-       console.log(
-        `Final Score\nPlayer: ${userScore}\nComputer ${computerScore}\nDraw: ${drawScore}`
-      );
-    }
-  
+  if (userWin) {
+    userScore += 1;
+    gameStatus.textContent = `You win :) this round\nYou chose ${userChoice},computer chose ${computerChoice}`;
+  } else if (computerWin) {
+    computerScore += 1;
+    gameStatus.textContent = `You lost :( this round\nYou chose ${userChoice},computer chose ${computerChoice}`;
+  }
+  if (draw) {
+    console.log(
+      `Final Score\nPlayer: ${userScore}\nComputer ${computerScore}\nDraw: ${drawScore}`
+    );
+  }
 }
 
 function playGame() {
@@ -108,23 +109,47 @@ function playGame() {
 
   showScore.textContent = `Player - ${userScore} | Computer - ${computerScore} | Draw- ${drawScore}`;
   rounds++;
-  if (rounds == 5){
+  if (rounds === round_left) {
+    winStatus.style.display = "block";
+
     if (userScore > computerScore) {
-      winStatus.textContent = ("User wins!");
-      winStatus.style.color = "green"
-  } else if (userScore < computerScore) {
-    winStatus.textContent = ("Computer wins!");
-    winStatus.style.color = "red"
-  } else {
-    winStatus.textContent = ("It's a tie!");
-  };
+      winStatus.textContent = "User wins!";
+      winStatus.style.color = "green";
+    } else if (userScore < computerScore) {
+      winStatus.textContent = "Computer wins!";
+      winStatus.style.color = "red";
+    } else {
+      winStatus.style.color = "black"
+      winStatus.textContent = "It's a tie!";
+    }
 
-  rock.disabled = true;
-  scissors.disabled = true;
-  paper.disabled = true;   
-  };
+    rock.disabled = true;
+    scissors.disabled = true;
+    paper.disabled = true;
 
-};
+    playAgain.style.display = "block";
+
+    
+  }
+}
+
+function resetGame() {
+  userScore = 0;
+  computerScore = 0;
+  drawScore = 0;
+  rounds = 0;
+
+  //
+  showScore.textContent = `Player - ${userScore} | Computer - ${computerScore} | Draw- ${drawScore}`;
+  gameStatus.textContent = "";
+
+  rock.disabled = false;
+  scissors.disabled = false;
+  paper.disabled = false;
+  //
+  winStatus.style.display = "none";
+  playAgain.style.display = "none";
+}
 
 function handleChoice(event) {
   getUserChoice(event);
@@ -134,3 +159,4 @@ function handleChoice(event) {
 rock.addEventListener("click", handleChoice);
 paper.addEventListener("click", handleChoice);
 scissors.addEventListener("click", handleChoice);
+playAgain.addEventListener("click", resetGame);
